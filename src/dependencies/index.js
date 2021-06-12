d7bf366aab1c38e5bb8381c32ef7feb56c360637
Marcos Volpi <marcos.volpi@BP4.com>
2021-06-12 09:07:55 -0300

    -Delete scaffolding examples -Add validation for add task

--- src/dependencies/index.js
+++ src/dependencies/index.js
@@ -1,26 +1,13 @@
-const serverController = require('../controllers/serverController');
-const messageController = require('../controllers/messageController');
-const serverRepository = require('../repository/serverRepository');
-const messageRepository = require('../repository/messageRepository');
+const taskController = require('../controllers/taskController');
 const taskRepository = require('../repository/taskRepository');
-const serverService = require('../services/serverService');
-const messageService = require('../services/messageService');
 const taskService = require('../services/taskService');
 
-
-const {Message, Server, Task, Sequelize, sequelize} = require('../models');
-const serverRepositoryOb=new serverRepository(Server);
-const serverServiceOb = new serverService(serverRepositoryOb);
-const serverControllerOb = new serverController(serverServiceOb);
-
-const messageRepositoryOb=new messageRepository(Message, Server, Sequelize, sequelize);
-const messageServiceOb=new messageService(messageRepositoryOb);
-const messageControllerOb = new messageController(messageServiceOb);
+const {Task, Sequelize, sequelize} = require('../models');
 
 const taskRepositoryOb=new taskRepository(Task);
 const taskServiceOb = new taskService(taskRepositoryOb);
-const taskControllerOb = new serverController(taskServiceOb);
+const taskControllerOb = new taskController(taskServiceOb);
 
 module.exports = {
-    Sequelize, sequelize, serverRepositoryOb, serverControllerOb, messageRepositoryOb, messageControllerOb, taskControllerOb
+    Sequelize, sequelize, taskControllerOb
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,53 +2,18 @@ const router = require('express').Router();
 const Joi = require('joi');
 const moment = require('moment');
 const {
-    serverRepositoryOb,
-    serverControllerOb,
-    messageControllerOb,
     taskControllerOb
 } = require('../dependencies/');
 
-router.get('/servers/', serverControllerOb.list);
-router.post('/servers/add/', addServerSchema, serverControllerOb.add);
-
-router.get('/messages/', messageControllerOb.list);
-router.get('/messagesByServer/:id', listByServer, messageControllerOb.listByServer);
-router.get('/messages/static', messageControllerOb.static);
-router.post('/messageByMessage', messageControllerOb.listByMessage);
-
 router.get('/tasks/', taskControllerOb.list);
-router.put('/tasks/', taskControllerOb.add);
+router.put('/tasks/', addTask, taskControllerOb.add);
 
-function listByServer(req, res, next){
+function addTask(req, res, next){
     const schema = Joi.object({
-        id: Joi.number().required()
+        description: Joi.string().required()
     });
     validateRequest(req, next, schema);
 }
-
-async function addServerSchema(req, res, next) {   
-    let params=req;
-    params.body.created_at=moment(moment(req.body.created_at, 'hh-mm-ss-DD-MM-YYYY')).format('YYYY-MM-DD hh:mm:ss');
-    let arrServers=[];
-
-    let servers = await serverRepositoryOb.list();
-    servers.forEach((s)=>{
-        arrServers.push(s.server);
-    });
-    
-    const schema = Joi.object({
-        server: Joi.string().required(),
-        server: Joi.string().invalid(...arrServers).messages({
-            'any.invalid': 'Server name can\'t be repeated'
-        }),
-        description: Joi.string().required(),
-        server_type: Joi.string().required(),
-        created_at: Joi.date().required()
-    });
-    validateRequest(params, next, schema);
-    
-}
-
 
 
 function validateRequest(req, next, schema) {
